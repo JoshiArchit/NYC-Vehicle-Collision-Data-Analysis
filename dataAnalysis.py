f332ad61ec7afa434a97b3a50a070fc79b0b50c7
architJoshi2904 <aj6082@rit.edu>
2023-12-01 22:21:59 -0500

    Pie chart legend

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -8,7 +8,9 @@ import warnings
 import psycopg2
 import pandas as pd
 from matplotlib import pyplot as plt
-from analyseData import seperateData
+from analyseData import separateData
+import matplotlib.patches as mpatches
+
 
 def connectDB():
     """
@@ -20,9 +22,9 @@ def connectDB():
     """
     host = 'localhost'
     username = 'postgres'
-    password = 'RITPostGreSQL'
+    password = 'Archit@2904'
     port = '5432'
-    database = 'NYC_Crash_Database'
+    database = 'db_720'
 
     try:
         # Attempt to connect to the existing database
@@ -58,6 +60,7 @@ def dayWithMostAccidents(connection):
     print("Answer:", result[0])
     print()
 
+
 def hourWithMostAccidents(connection):
     """
     This function will find the hour of the day which has most accidents
@@ -81,6 +84,7 @@ def hourWithMostAccidents(connection):
     print("6. Which hour of the day has the most accidents?")
     print("Answer:", result[0])
     print()
+
 
 def twelveDaysWithMostAccidentsIn2020(connection):
     """
@@ -113,11 +117,13 @@ def twelveDaysWithMostAccidentsIn2020(connection):
     # removing the name of month from the date
     for days in result:
         if 'June' in days[0]:
-            june.append(days[0].replace("June ",""))
+            june.append(days[0].replace("June ", ""))
         elif "July" in days[0]:
-            july.append(days[0].replace("July ",""))
-    print("7. In the year 2020, which 12 days had the most accidents?\nCan you speculate about why this is?")
-    print("Ans: ",", ".join(day for day in june),"June", "\n     ", ", ".join(day for day in july), "July")
+            july.append(days[0].replace("July ", ""))
+    print(
+        "7. In the year 2020, which 12 days had the most accidents?\nCan you speculate about why this is?")
+    print("Ans: ", ", ".join(day for day in june), "June", "\n     ",
+          ", ".join(day for day in july), "July")
 
 
 def top100ConsecutiveDaysWithMostAccidents(connection):
@@ -143,7 +149,8 @@ def top100ConsecutiveDaysWithMostAccidents(connection):
         return False
     connection.cursor().close()
 
-    print("4. For the year of January 2019 to October of 2020, which 100 consecutive days had the most accidents?")
+    print(
+        "4. For the year of January 2019 to October of 2020, which 100 consecutive days had the most accidents?")
     all_days = []
     accidents_in_a_day = []
     max_accidents = 0
@@ -155,11 +162,14 @@ def top100ConsecutiveDaysWithMostAccidents(connection):
         accidents_in_a_day.append(values[1])
 
     for index in range(0, 122):
-        total_accidents_in_current_range = sum(accidents_in_a_day[index:index+100])
+        total_accidents_in_current_range = sum(
+            accidents_in_a_day[index:index + 100])
         if total_accidents_in_current_range > max_accidents:
             max_accidents = total_accidents_in_current_range
             start_index_for_max_accidents = index
-    print("Answer: The 100 consecutive days with most accidents start from", all_days[start_index_for_max_accidents], "till", all_days[start_index_for_max_accidents+99])
+    print("Answer: The 100 consecutive days with most accidents start from",
+          all_days[start_index_for_max_accidents], "till",
+          all_days[start_index_for_max_accidents + 99])
     print()
 
 
@@ -178,44 +188,66 @@ def categorize_time_frame(hour):
     elif 18 <= hour <= 24:
         return 'Evening'
 
+
 def dataChangeByTimeFrameFromTwoYears(crash_data_2019, crash_data_2020):
     """
     This function will find out what changed in two years based on time and will create a pie chart
     :param crash_data_2019: crash data from year 2019
     :param crash_data_2020: crash data from year 2020
     """
+    # Create legend
+    legend_patches = [
+        mpatches.Patch(color='orange', label='Morning --> 06:00 to 11:59'),
+        mpatches.Patch(color='blue', label='Afternoon --> 12:00 to 17:59 '),
+        mpatches.Patch(color='green', label='Evening --> 18:00 to 23:59'),
+        mpatches.Patch(color='red', label='Night --> 00:00 to 05:59')
+    ]
+
     # converting the varchar data format to datetime
-    crash_data_2019['crash_time'] = pd.to_datetime(crash_data_2019['crash_time'])
+    crash_data_2019['crash_time'] = pd.to_datetime(
+        crash_data_2019['crash_time'])
 
     # getting the hour from the crash time
-    crash_data_2019['time_frame'] = crash_data_2019['crash_time'].dt.hour.apply(categorize_time_frame)
+    crash_data_2019['time_frame'] = crash_data_2019[
+        'crash_time'].dt.hour.apply(categorize_time_frame)
 
     # performing aggregation and getting percentage of crash in each time frame
     result2019 = crash_data_2019.groupby('time_frame').agg(
         accident_count=('crash_time', 'count'),
-        percentage=('crash_time', lambda x: round(len(x) * 100.0 / len(crash_data_2019), 1))
+        percentage=('crash_time',
+                    lambda x: round(len(x) * 100.0 / len(crash_data_2019), 1))
     ).reset_index()
 
     result2019 = result2019.sort_values(by='percentage', ascending=False)
 
     # 2020 data
-    crash_data_2020['crash_time'] = pd.to_datetime(crash_data_2020['crash_time'])
-    crash_data_2020['time_frame'] = crash_data_2020['crash_time'].dt.hour.apply(categorize_time_frame)
+    crash_data_2020['crash_time'] = pd.to_datetime(
+        crash_data_2020['crash_time'])
+    crash_data_2020['time_frame'] = crash_data_2020[
+        'crash_time'].dt.hour.apply(categorize_time_frame)
     result2020 = crash_data_2020.groupby('time_frame').agg(
         accident_count=('crash_time', 'count'),
-        percentage=('crash_time', lambda x: round(len(x) * 100.0 / len(crash_data_2020), 1))
+        percentage=('crash_time',
+                    lambda x: round(len(x) * 100.0 / len(crash_data_2020), 1))
     ).reset_index()
 
     result2020 = result2020.sort_values(by='percentage', ascending=False)
 
+    # Generating pie charts
     fig, axs = plt.subplots(1, 2, figsize=(16, 8))
     # pie chart for 2019
-    axs[0].pie(result2019['percentage'], labels=result2019['time_frame'], autopct='%1.1f%%', startangle=90)
+    axs[0].pie(result2019['percentage'], labels=result2019['time_frame'],
+               autopct='%1.1f%%', startangle=90)
     axs[0].set_title('Distribution of Accidents by Time Frame (2019)')
 
     # Pie chart for 2020
-    axs[1].pie(result2020['percentage'], labels=result2020['time_frame'], autopct='%1.1f%%', startangle=90)
+    axs[1].pie(result2020['percentage'], labels=result2020['time_frame'],
+               autopct='%1.1f%%', startangle=90)
     axs[1].set_title('Distribution of Accidents by Time Frame (2020)')
+
+    # Add legend
+    plt.legend(handles=legend_patches, title="Time Blocks", loc="upper left",
+               bbox_to_anchor=(0, 1), bbox_transform=plt.gcf().transFigure)
 
     plt.show()
 
@@ -230,20 +262,29 @@ def dataChangeByZipcodeFromTwoYears(crash_data_2019, crash_data_2020):
     crash_data_2019['year'] = crash_data_2019['crash_date'].dt.year
     crash_data_2020['year'] = crash_data_2020['crash_date'].dt.year
 
-    region_accidents_count_2019 = crash_data_2019.groupby(['zip_code', 'year']).size().reset_index(name='count')
-    region_accidents_count_2020 = crash_data_2020.groupby(['zip_code', 'year']).size().reset_index(name='count')
+    region_accidents_count_2019 = crash_data_2019.groupby(
+        ['zip_code', 'year']).size().reset_index(name='count')
+    region_accidents_count_2020 = crash_data_2020.groupby(
+        ['zip_code', 'year']).size().reset_index(name='count')
 
-    plot_time_series_for_accidents(region_accidents_count_2019, region_accidents_count_2020)
+    plot_time_series_for_accidents(region_accidents_count_2019,
+                                   region_accidents_count_2020)
 
-def plot_time_series_for_accidents(region_accidents_count_2019, region_accidents_count_2020):
+
+def plot_time_series_for_accidents(region_accidents_count_2019,
+                                   region_accidents_count_2020):
     """
     This function will create plot time series for accidents for the year 2019 and 2020
     :param region_accidents_count_2019: accidents count in 2019 segregated by region
     :param region_accidents_count_2020: accidents count in 2020 segregated by region
     """
     plt.figure(figsize=(10, 6))
-    plt.plot(region_accidents_count_2019['zip_code'], region_accidents_count_2019['count'], label='2019 Accidents', marker='o')
-    plt.plot(region_accidents_count_2020['zip_code'], region_accidents_count_2020['count'], label='2020 Accidents', marker='o')
+    plt.plot(region_accidents_count_2019['zip_code'],
+             region_accidents_count_2019['count'], label='2019 Accidents',
+             marker='o')
+    plt.plot(region_accidents_count_2020['zip_code'],
+             region_accidents_count_2020['count'], label='2020 Accidents',
+             marker='o')
 
     plt.title('Accidents Over Time (2019 vs 2020)')
     plt.xlabel('Zip Code')
@@ -253,20 +294,23 @@ def plot_time_series_for_accidents(region_accidents_count_2019, region_accidents
     plt.grid(True)
     plt.show()
 
+
 def main():
     """
     This is the main function
     """
     conn = connectDB()
-    warnings.filterwarnings("ignore", message="pandas only supports SQLAlchemy connectable.*")
+    warnings.filterwarnings("ignore",
+                            message="pandas only supports SQLAlchemy connectable.*")
     dataframe = pd.read_sql("SELECT * FROM nyc_crashes", conn)
-    crash_data_2019, crash_data_2020 = seperateData(dataframe)
-    dataChangeByZipcodeFromTwoYears(crash_data_2019, crash_data_2020)
+    crash_data_2019, crash_data_2020 = separateData(dataframe)
+    # dataChangeByZipcodeFromTwoYears(crash_data_2019, crash_data_2020)
     dataChangeByTimeFrameFromTwoYears(crash_data_2019, crash_data_2020)
-    top100ConsecutiveDaysWithMostAccidents(conn)
-    dayWithMostAccidents(conn)
-    hourWithMostAccidents(conn)
-    twelveDaysWithMostAccidentsIn2020(conn)
+    # top100ConsecutiveDaysWithMostAccidents(conn)
+    # dayWithMostAccidents(conn)
+    # hourWithMostAccidents(conn)
+    # twelveDaysWithMostAccidentsIn2020(conn)
+
 
 if __name__ == '__main__':
     main()
